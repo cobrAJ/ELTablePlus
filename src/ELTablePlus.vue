5e0cbd576a7ad25f337fdb8ff6265001627c1851
Jun An <anjunn@163.com>
2022-01-14 11:37:55 +0800

    横向无限滚动

--- src/ELTablePlus.vue
+++ src/ELTablePlus.vue
@@ -1,4 +1,5 @@
 <script>
+import lodash from "lodash"
 // import massData from "@/json/massData2.json"
 export default {
   name: "ELTablePlus",
@@ -13,16 +14,22 @@ export default {
       },
       massDataTableDom: '',
       lastScrollTop: "",
+      lastScrollLeft: "",
       lastSign: "",
       selfMaxHeight: 500,
       selectPageNumChange: false,//阻止多余的selectchange事件触发
       selectionPageMap: [],//已选择的选项
-      nextPartData: []//下一个页面的数据（包括上翻，下翻）
+      nextPartData: [],//下一个页面的数据（包括上翻，下翻）
+      currentSlotDefault: [],
+      needColumnPage: false,
+      columnPages: [],
+      colCurrentPage: 1
     }
   },
   render (h) {
     let finalObj = {};
     if (typeof h != "function") {
+      //vue3 代码兼容处理，但是el-table 组件并不兼容
       h = require("vue").h;
       finalObj.attrs = {
         ...this.$attrs,
@@ -40,9 +47,46 @@ export default {
       };
       finalObj.on = { ...this.$listeners, select: this.selfSelect, "selection-change": this.selfSelectionChange };
     }
-    return h("el-table", finalObj, this.$slots.default)
+    return h("el-table", finalObj, this.currentSlotDefault)
   },
   methods: {
+    columnDataSplit () {
+      let tmp = _.clone(this.$slots.default);
+      let columnMinWidth = 0;
+      this.columnPages = [];
+      tmp.forEach(col => {
+        if (col.width) {
+          columnMinWidth += parseFloat(col.width)
+        } else {
+          columnMinWidth += 80
+        }
+        let index = parseInt(columnMinWidth / window.innerWidth);
+        this.columnPages[index] ? '' : this.columnPages[index] = [];
+        this.columnPages[index].push(col);
+      });
+    },
+    columnRenderCheck () {
+      let tmp = _.clone(this.$slots.default);
+      let outPutTmp = [];
+      let columnMinWidth = 0;
+      tmp.forEach(col => {
+        if (col.width) {
+          columnMinWidth += parseFloat(col.width)
+        } else {
+          columnMinWidth += 80
+        }
+        // 大于屏幕宽度两倍的列数据不渲染
+        if (columnMinWidth < window.innerWidth * 2) {
+          outPutTmp.push(col)
+        } else {
+          this.needColumnPage = true
+        }
+      });
+      if (this.needColumnPage) {
+        this.columnDataSplit();
+      }
+      return outPutTmp
+    },
     selfSelect (selected, row) {
       let signIndex = -1;
       this.selectionPageMap.forEach((item, index) => {
@@ -77,31 +121,14 @@ export default {
         })
       }
     },
-    scrollDebounce (fn, delay, isImmediate) {
-      var timer = null;  //初始化timer，作为计时清除依据
-      return function () {
-        var context = this;  //获取函数所在作用域this
-        var args = arguments;  //取得传入参数
-        clearTimeout(timer);
-        if (isImmediate && timer === null) {
-          //时间间隔外立即执行
-          fn.apply(context, args);
-          timer = 0;
-          return;
-        }
-        timer = setTimeout(function () {
-          fn.apply(context, args);
-          timer = null;
-        }, delay);
-      }
-    },
     dataSplit (pageNum) {
       return this.massData.slice(((pageNum || this.pagination.currentPage) - 1) * this.pagination.pageSize, (pageNum || this.pagination.currentPage) * this.pagination.pageSize);
     },
     scrollhandler () {
-      // console.log("scrollTop:", this.massDataTableDom.scrollTop, "lastScrollTop:", this.lastScrollTop);
-      let needFresh = true, sign = this.massDataTableDom.scrollTop > this.lastScrollTop ? 'down'
-        : this.massDataTableDom.scrollTop < this.lastScrollTop ? "up" : this.lastSign;
+      // console.log("scrollLeft:", this.massDataTableDom.scrollLeft, "lastScrollLeft:", this.lastScrollLeft);
+      let needVerticalFresh = true, needHorizontalFresh = true, sign = this.massDataTableDom.scrollTop > this.lastScrollTop ? 'down'
+        : this.massDataTableDom.scrollTop < this.lastScrollTop ? "up" : this.massDataTableDom.scrollLeft > this.lastScrollLeft ? 'right' :
+          this.massDataTableDom.scrollLeft < this.lastScrollLeft ? 'left' : this.lastSign;
       // console.log("scrollTop:", this.massDataTableDom.scrollTop, "offsetHeight:", this.massDataTableDom.offsetHeight, "scrollHeight:", this.massDataTableDom.scrollHeight, "sign:", sign);
       if (this.pagination.currentPage > 2 && this.massDataTableDom.scrollTop == 0 && sign == "up") {
         this.pagination.currentPage -= 1;
@@ -110,9 +137,16 @@ export default {
         this.pagination.currentPage += 1;
         this.selectPageNumChange = true;
       } else {
-        needFresh = false
+        needVerticalFresh = false
+        if (this.needColumnPage && sign == "left" && this.massDataTableDom.scrollLeft == 0 && this.colCurrentPage > 1) {
+          this.colCurrentPage -= 1
+        } else if (this.needColumnPage && sign == "right" && this.massDataTableDom.scrollLeft >= (window.innerWidth - 80)) {
+          this.colCurrentPage += 1
+        } else {
+          needHorizontalFresh = false;
+        }
       }
-      if (needFresh) {
+      if (needVerticalFresh) {
         let oldScrollTop = this.massDataTableDom.scrollTop;
         let oldScrollHeight = this.massDataTableDom.scrollHeight;
         if (sign == "up") {
@@ -142,11 +176,24 @@ export default {
           }
         })
       }
+      if (needHorizontalFresh) {
+        console.log(this.columnPages[this.colCurrentPage - 1].concat(this.columnPages[this.colCurrentPage]));
+        this.currentSlotDefault = this.columnPages[this.colCurrentPage - 1].concat(this.columnPages[this.colCurrentPage]);
+        this.$nextTick(() => {
+          if (sign == 'right') {
+            this.massDataTableDom.scrollLeft = 0;
+          } else if (sign == 'left') {
+            this.massDataTableDom.scrollLeft = window.innerWidth - 80;
+          }
+        })
+      }
       this.lastSign = sign;
       this.lastScrollTop = this.massDataTableDom.scrollTop;
+      this.lastScrollLeft = this.massDataTableDom.scrollLeft;
     }
   },
   mounted () {
+    this.currentSlotDefault = this.columnRenderCheck(0)
     // console.log("pagination.pageSize:", this.pagination.pageSize)
     if (this.$attrs.data && this.$attrs.data.length > 0) {
       this.massData = JSON.parse(JSON.stringify(this.$attrs.data));
@@ -156,7 +203,7 @@ export default {
       this.pagination.currentPage += 1
     }
     this.massDataTableDom = document.querySelector('.el-table__body-wrapper');
-    this.massDataTableDom.onscroll = this.scrollhandler
+    this.massDataTableDom.onscroll = lodash.debounce(this.scrollhandler, 100)
   },
 }
 </script>
